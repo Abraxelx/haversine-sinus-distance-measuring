a8ce3c710e4dfb2b50a799e761d1031fff07b571
Halil <halil.sahin@datassist.com.tr>
2020-05-29 04:52:30 +0300

    added haversine lib

--- ReadExcel.py
+++ ReadExcel.py
@@ -1,9 +1,11 @@
 import pandas as pd
+from haversine import haversine, Unit
 
 excelFile = pd.read_excel("haversine.xlsx")
 excelFile.to_csv('datas.csv',index = None, header=True)
-df = pd.read_csv("datas.csv", usecols=[3,4,6,7,8,9])
-print(df)
-#dataFrame =pd.DataFrame(excelFile)
-#print(dataFrame)
-#excelFile.get_value(3, 4)
+df = pd.read_csv("datas.csv", usecols=[3,4])
+paris =(df['Latitude (N)'].values[4], df['Longitude (E)'].values[4])
+istanbul = (df['Latitude (N)'].values[5], df['Longitude (E)'].values[5])
+
+print(haversine(paris,istanbul, unit=Unit.MILES))
+print(df.shape[0])
