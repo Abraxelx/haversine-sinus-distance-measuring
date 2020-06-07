dd76273b2bfba73659a7a085a43bbfb49294e0a0
Halil <halil.sahin@datassist.com.tr>
2020-06-07 20:38:09 +0300

    sequence and subset file optimized.

--- ApplyHaversine.py
+++ ApplyHaversine.py
@@ -1,37 +1,52 @@
 import pandas as pd
+import os
 import csv
 from haversine import haversine, Unit
 
-#Okunacak Excel Adı:
-excelFile = pd.read_excel("haversine.xlsx")
-#Excel csv dosyasına dönüştürülüyor
+# Excel file name:
+dir = 'files'
+excelFilex = os.path.join(dir, 'haversine.xlsx')
+excelFile = pd.read_excel(excelFilex)
+#Converting excel file to csv
 excelFile.to_csv('datas.csv',index = None, header=True)
-#Excel dosyasındaki lat ve long konumlarunı usecols'daki değerler içinde belirtin. Saymaya 0'dan başlayın
+#Latitudes and Longitudes columns locations in excel file. (First col is = 0)
 df = pd.read_csv("datas.csv", usecols=[3,4])
+
+#Column names of Lat and Longs
+latColumnName = 'Latitude (N)'
+longColumnName =  'Longitude (E)'
 
 
 length = df.shape[0]
 
 with open('subset.csv', 'w', newline='') as f:
-    fieldnames =['start','end','value']
+    fieldnames =['start','end','Lat1','Long1','Lat2','Long2','value']
     thewriter = csv.DictWriter(f, fieldnames=fieldnames)
     thewriter.writeheader()
     for i in range(length):
         for j in range(length):
-            location_1 =(df['Latitude (N)'].values[i], df['Longitude (E)'].values[i])
-            location_2 = (df['Latitude (N)'].values[j], df['Longitude (E)'].values[j])
-            thewriter.writerow({'start' : i, 'end' : j, 'value' : haversine(location_1,location_2, unit=Unit.KILOMETERS)})
+            lat1 = df[latColumnName].values[i]
+            long1 = df[longColumnName].values[i]
+            lat2=  df[latColumnName].values[j]
+            long2= df[longColumnName].values[j]
+            location_1 =(lat1,long1 )
+            location_2 = (lat2, long2 )
+            thewriter.writerow({'start' : i, 'end' : j, 'Lat1' : lat1, 'Long1' : long1, 'Lat2' : lat2, 'Long2' : long2, 'value' : haversine(location_1,location_2, unit=Unit.KILOMETERS)})
 
 
 with open('sequence.csv', 'w', newline='') as f:
-    fieldnames =['start','end','value']
+    fieldnames =['start','end','Lat1','Long1','Lat2','Long2','value']
     thewriter = csv.DictWriter(f, fieldnames=fieldnames)
     thewriter.writeheader()
     for i in range(length):
 
         if not i == (length-1):
-            paris =(df['Latitude (N)'].values[i], df['Longitude (E)'].values[i])
-            istanbul = (df['Latitude (N)'].values[i+1], df['Longitude (E)'].values[i+1])
-            thewriter.writerow({'start' : i, 'end' : i+1, 'value' : haversine(paris,istanbul, unit=Unit.KILOMETERS)})
+            lat1= df[latColumnName].values[i]
+            long1= df[longColumnName].values[i]
+            lat2= df[latColumnName].values[i+1]
+            long2= df[longColumnName].values[i+1]
+            location_1 =(lat1,long1)
+            location_2 = (lat2, long2)
+            thewriter.writerow({'start' : i, 'end' : i+1, 'Lat1' : lat1, 'Long1' : long1, 'Lat2' : lat2, 'Long2' : long2, 'value' : haversine(location_1,location_2, unit=Unit.KILOMETERS)})
         else:
             print("Sequence File Created")
